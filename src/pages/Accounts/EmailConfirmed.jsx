5a27daa286ef52b974993274a69301c36b6333c8
Carthur83 <christian_A_83@hotmail.com>
2025-06-03 13:47:07 +0200

    added staticwebapp.config

--- src/pages/Accounts/EmailConfirmed.jsx
+++ src/pages/Accounts/EmailConfirmed.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from 'react-router-dom';
-import './Emailconfirmed.css'
+import './EmailConfirmed.css';
 
 function EmailConfirmed() {
 
